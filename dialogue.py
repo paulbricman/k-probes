a71bf55df503ef07b6cdd447fe7055ea7d98beff
suHarvey <81810954+suHarvey@users.noreply.github.com>
2021-04-02 12:57:20 +0100

    Update dialogue.py

--- dialogue.py
+++ dialogue.py
@@ -1,7 +1,9 @@
+import os
 import json
 import random
+import sys
 
-prompts = json.load(open('prompts.json'))
+prompts = json.load(open("prompts.json"))
 
 configuration = {
     "Remember": 10,
@@ -13,9 +15,13 @@ configuration = {
 }
 
 while True:
-    input()
+    answer = input()
 
     sample_category = random.choices(list(configuration.keys()), weights = list(configuration.values()))[0]
     sample_prompt = random.choice(prompts[sample_category])
 
     print('\n' + sample_prompt)
+    
+    res = open(os.path.join(sys.path[0], "Export.txt"), "a")
+    res.write(sample_prompt + "\n" + answer + "\n\n\n")
+    res.close()
